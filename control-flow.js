34efe2cccef4d4da129fd3b2c5fb6fba9dd857b8
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-03-12 22:15:09 +0500

    add

---
+++ control-flow.js
@@ -0,0 +1,98 @@
+//if statement
+//if(true){
+    // code
+// }
+
+// const isuserlogin = true
+
+// if(isuserlogin){
+//     console.log("execute");
+// }
+
+// const u=50
+// if(u>=40){
+//     console.log("program execute");
+// }
+// else{
+//     console.log("program not execute");
+// }
+
+//NESTING*************
+
+// let temperature = 25;
+// let humidity = 80;
+
+// if (temperature > 30) {
+//     if (humidity > 70) {
+//         console.log("It's hot and humid. Stay indoors.");
+//     } else {
+//         console.log("It's hot but not too humid. Enjoy the sun!");
+//     }
+// } else if (temperature <= 30 && temperature >= 20) {
+//     console.log("The weather is pleasant.");
+// } else {
+//     console.log("It's cool outside.");
+// }
+
+//&& operator
+const userlogin = true
+const user = true
+const uname =false
+const email = true
+if(userlogin && user){
+    // console.log("allow to buy");
+}
+// || operator
+
+if(uname || email){
+    // console.log("user login")
+}
+
+//falsy values
+
+// false , 0 , -0, Bigint, "" , null, undefiened, NAN
+
+//truthy value
+//"0", 'false', " ", [], {}, function(){}, 
+
+
+
+// Nullish Coa;escing operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+
+// console.log(val1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
