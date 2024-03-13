d6cd25737575af8300b1a163a3bd3d63c5127b11
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-03-13 13:26:07 +0500

    loops in js

---
+++ loops.js
@@ -0,0 +1,77 @@
+//for loops
+
+// for (let i = 0; i < 10; i++) {
+//     // console.log(i)
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log(`outter lopp: ${i}`);
+//     for(let j = 0; j< 3; j++)
+//     console.log(`inner loop: ${j}`);
+    
+// }
+//TABLE
+
+// for (let i = 2; i <=10; i++) {
+// //   console.log(`Table Of: ${i}`);
+//      for(let j = 1; j<=10; j++){
+//     //  console.log(i + '*' + j + ' = ' + i*j);
+//      }
+//     }
+//break
+
+// for(i=0;i<10;i++){
+//     if(i==5){
+//         console.log("i am out");
+//         break
+//     }
+//     console.log(`my number is: ${i}`)
+// }
+
+// //continue
+
+// for(i=0;i<10;i++){
+//     if(i==5){
+//         console.log("i am 1s out");
+//         continue
+//     }
+//     console.log(`my number is: ${i}`)
+// }
+
+
+//******************** while loops */*****************
+
+
+// let i =1
+// while (i<=10) {
+//     // console.log(i)
+//     i++
+// }
+// let marray = ["abc", "def", "ghi"]
+// let arr = 0
+// while(arr < marray.length){
+//     console.log(`value is ${marray[arr]}`);
+//     arr = arr + 1
+// }
+
+// *************************************do while**************
+
+
+// let score = 1
+// do {
+//     console.log(`score is: ${score}`);
+//     score++
+// } while (score<10);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
